c20dc8300b5032ac2c5dde0fe0fb67764c67b2f0
Satyaa03 <satyaa.courses@gmail.com>
2025-06-02 20:02:27 +0530

    Fifth Commit

--- src/main/java/org/cris/railways/module/service/StationService.java
+++ src/main/java/org/cris/railways/module/service/StationService.java
@@ -1,17 +1,42 @@
 package org.cris.railways.module.service;
 
+import org.cris.railways.module.dao.StationDao;
 import org.cris.railways.module.model.Station;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
-
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface StationService {
-    void addStation(Station station) throws SQLException;
-    Station getStation(String stnCode) throws SQLException;
-    List<Station> getAllStations() throws SQLException;
-    void updateStation(Station station) throws SQLException;
-    void deleteStation(String stnCode) throws SQLException;
+@Service
+public class StationService {
 
+    private final StationDao stationDao;
+
+    @Autowired
+    public StationService(DataSource dataSource) throws SQLException {
+        // Initialize StationDao with a DB connection
+        this.stationDao = new StationDao(dataSource.getConnection());
+    }
+
+    public void addStation(Station station) throws SQLException {
+        stationDao.addStation(station);
+    }
+
+    public Station getStation(String stnCode) throws SQLException {
+        return stationDao.getStation(stnCode);
+    }
+
+    public List<Station> getAllStations() throws SQLException {
+        return stationDao.getAllStations();
+    }
+
+    public void updateStation(Station station) throws SQLException {
+        stationDao.updateStation(station);
+    }
+
+    public void deleteStation(String stnCode) throws SQLException {
+        stationDao.deleteStation(stnCode);
+    }
 }
